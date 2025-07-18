a745cb71562f34c620bb1156ce7d2193f185dfa0
Xenthio <thegooster88@gmail.com>
2025-07-18 10:49:06 +1000

    Add playercomponent

---
+++ code/Player/PlayerComponent.cs
@@ -0,0 +1,13 @@
+﻿using XMovement;
+
+namespace XPSBase;
+
+public class PlayerComponent : Component
+{
+	[Property, Group( "Player" )] public PlayerWalkControllerComplex WalkController;
+	[Property, Group( "Player" )] public PlayerMovement Movement;
+	protected override void OnStart()
+	{
+		base.OnStart();
+	}
+}
