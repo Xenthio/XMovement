cff30c4399e173f249bb7d0c05b6c1d294ee7c00
Xenthio <thegooster88@gmail.com>
2025-07-18 11:00:10 +1000

    Basic Health Stuff

--- code/Player/PlayerComponent.cs
+++ code/Player/PlayerComponent.cs
@@ -2,12 +2,23 @@
 
 namespace XPSBase;
 
-public class PlayerComponent : Component
+public class PlayerComponent : Component, Component.IDamageable
 {
 	[Property, Group( "Player" )] public PlayerWalkControllerComplex WalkController;
 	[Property, Group( "Player" )] public PlayerMovement Movement;
+	[Property, Group( "Life" )] public float Health = 100f;
 	protected override void OnStart()
 	{
 		base.OnStart();
+	}
+
+	public void OnDamage( in DamageInfo damage )
+	{
+		Health -= damage.Damage;
+		if ( Health <= 0 )
+		{
+			Health = 0;
+			Log.Info( "Player has died." );
+		}
 	}
 }
