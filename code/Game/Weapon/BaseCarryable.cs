b4c1e43eb3a683e27dccb55babbcd1ebc563dc21
Xenthio <thegooster88@gmail.com>
2025-07-22 13:59:52 +1000

    Add stub player inventory

--- code/Game/Weapon/BaseCarryable.cs
+++ code/Game/Weapon/BaseCarryable.cs
@@ -1,5 +1,6 @@
 ﻿
 public partial class BaseCarryable : Component
 {
-
+	[Property, Feature( "Inventory" ), Range( 0, 6 )] public int InventorySlot { get; set; } = 0;
+	[Property, Feature( "Inventory" )] public int InventoryOrder { get; set; } = 0;
 }
---
+++ code/Player/PlayerInventory.cs
@@ -0,0 +1,6 @@
+﻿public class PlayerInventory : Component
+{
+	[RequireComponent] public Player Player { get; set; }
+	public List<BaseCarryable> Weapons => GetComponentsInChildren<BaseCarryable>( true ).OrderBy( x => x.InventorySlot ).ThenBy( x => x.InventoryOrder ).ToList();
+	[Sync] public BaseCarryable ActiveWeapon { get; private set; }
+}
